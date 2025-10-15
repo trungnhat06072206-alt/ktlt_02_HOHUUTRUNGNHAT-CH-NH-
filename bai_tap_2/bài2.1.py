ae7de7b01550aa37f6fdb0316d32f8a517198ae5
trungnhat06072206-alt <phamtuandibar@gmail.com>
2025-10-15 16:26:47 +0700

    Add files via upload

--- bai_tap_2/bài2.1.py
+++ bai_tap_2/bài2.1.py
@@ -1,1 +1,11 @@
+# Bài 1: So sánh hai số nguyên
 
+n1 = int(input("Enter n1 value: "))
+n2 = int(input("Enter n2 value: "))
+
+if n1 > n2:
+    print("n1 is big")
+elif n1 < n2:
+    print("n2 is big")
+else:
+    print("Both are equal")
